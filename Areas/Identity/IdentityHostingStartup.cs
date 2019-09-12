62563e0d8dc6e752641657888986f5e349800591
Ravindra Saxena <ravindra_s@HCL.COM>
2019-09-12 18:49:32 +0530

    Add project files.

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Web2FA.Data;
+
+[assembly: HostingStartup(typeof(Web2FA.Areas.Identity.IdentityHostingStartup))]
+namespace Web2FA.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
